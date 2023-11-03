f18a23c1ac8dd2eb8bbc68210fcbb614ae46613b
Gazi Yeasin Arifin <80382958+GaziYeasinArifin@users.noreply.github.com>
2023-11-02 19:04:48 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,113 @@
+let table;
+let bubbles = [];
+let padding = 100;
+
+function preload() {
+  table = loadTable('https://docs.google.com/spreadsheets/d/e/2PACX-1vTvE21yVlpKEFkrZebNN-yhxDlwMMPte1eS7DyUjtwRf3Iywf4KAcKG22NmN00ZFsC-oudSKRx3gdA_/pub?output=csv', 'csv', 'header');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let row of table.rows) {
+    let car = row.getString('Car');
+    let topSpeed = row.getNum('Top Speed (mph)');
+    let acceleration = row.getNum('0-60 mph (seconds)');
+    let manufacturer = row.getString('Manufacturer');
+    let diameter = map(topSpeed, 0, 350, 30, 250); // Slightly larger bubbles
+    let x, y;
+    do {
+      x = random(width);
+      y = random(height);
+    } while (isOverlapping(x, y, diameter));
+    let bubble = new Bubble(x, y, diameter, car, topSpeed, acceleration, manufacturer);
+    bubbles.push(bubble);
+  }
+}
+
+function draw() {
+  background(0);
+  for (let bubble of bubbles) {
+    bubble.crossfade();
+    bubble.display();
+    bubble.move(bubbles); // Pass the array of bubbles for collision detection
+  }
+}
+
+function isOverlapping(x, y, diameter) {
+  for (let bubble of bubbles) {
+    let d = dist(x, y, bubble.x, bubble.y);
+    if (d < (diameter + bubble.diameter) / 2) {
+      return true;
+    }
+  }
+  return false;
+}
+
+class Bubble {
+  constructor(x, y, diameter, car, topSpeed, acceleration, manufacturer) {
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.car = car;
+    this.topSpeed = topSpeed;
+    this.acceleration = acceleration;
+    this.manufacturer = manufacturer;
+    this.fade = random(255);
+    this.fadeStep = random(1, 5); // Slower color change
+    this.angle = random(TWO_PI);
+    this.speed = random(0.2, 2); // Slower movement
+  }
+
+  crossfade() {
+    this.fade += this.fadeStep;
+    if (this.fade >= 255 || this.fade <= 0) {
+      this.fadeStep *= -1;
+    }
+  }
+
+  display() {
+    noStroke();
+    fill(this.fade, 40, 225 - this.fade, 225);
+    ellipse(this.x, this.y, this.diameter);
+    textSize(15);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    textStyle(BOLD);
+    text(this.car, this.x, this.y - 20);
+    textStyle(NORMAL);
+    textSize(12);
+    text(`Top Speed: ${this.topSpeed} mph`, this.x, this.y + 20);
+    text(`Acceleration: ${this.acceleration} sec`, this.x, this.y + 40);
+    text(`Manufacturer: ${this.manufacturer}`, this.x, this.y);
+  }
+
+  move(allBubbles) {
+    let dx = cos(this.angle) * this.speed;
+    let dy = sin(this.angle) * this.speed;
+
+    // Check for collisions with other bubbles
+    for (let other of allBubbles) {
+      if (other !== this) {
+        let d = dist(this.x + dx, this.y + dy, other.x, other.y);
+        if (d < (this.diameter + other.diameter) / 2) {
+          // Change direction upon collision
+          this.angle = random(TWO_PI);
+          break; // Exit the loop after the first collision
+        }
+      }
+    }
+
+    // Check for collisions with the screen edges
+    if (this.x - this.diameter / 2 <= 0 || this.x + this.diameter / 2 >= width) {
+      this.angle = PI - this.angle;
+    }
+
+    if (this.y - this.diameter / 2 <= 0 || this.y + this.diameter / 2 >= height) {
+      this.angle = -this.angle;
+    }
+
+    // Update the position
+    this.x += cos(this.angle) * this.speed;
+    this.y += sin(this.angle) * this.speed;
+  }
+}
